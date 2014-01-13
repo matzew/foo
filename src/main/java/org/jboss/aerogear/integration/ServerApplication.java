1a62228250204daa72b4114645e2b7b6431162a0
Daniel Passos <daniel@passos.me>
2014-01-13 12:22:32 -0200

    Create project skeleton

---
+++ src/main/java/org/jboss/aerogear/integration/ServerApplication.java
@@ -0,0 +1,8 @@
+package org.jboss.aerogear.integration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ServerApplication extends Application {
+}
